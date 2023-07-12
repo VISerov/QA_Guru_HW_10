77bd32239819708e6fec080e9991fc3d97a07701
Vladimir Serov <serov.qa@gmail.com>
2023-07-12 15:17:04 +0300

    Выполнил 1 часть ДЗ

--- pages/registration_form.py
+++ pages/registration_form.py
@@ -1,10 +1,12 @@
-from selene import browser, have, be
-import os
-import time
+from selene import browser, have, command
+
 from tests.conftest import path
 
 
 class RegistrationPage:
+    def __init__(self):
+        self.state = browser.element('#state')
+
     def open(self):
         browser.open('/automation-practice-form')
 
@@ -26,6 +28,7 @@ class RegistrationPage:
 
     def fill_mobile(self, value):
         browser.element('[id="userNumber"]').type(value)
+        return self
 
     def fill_date_of_birth(self, year, day, month):
         browser.element('#dateOfBirthInput').click()
@@ -51,10 +54,38 @@ class RegistrationPage:
         browser.element('#uploadPicture').send_keys(path(file_name))
         return self
     #
-    # def fill_first_name(self, value):
-    #     browser.element('#currentAddress').type('Novgorod')
-    #     browser.element('#state').click()
-    #     browser.all("#state div").element_by(have.exact_text("NCR")).click()
-    #     browser.element('#city').click()
-    #     browser.all("#city div").element_by(have.exact_text("Delhi")).click()
-    #     browser.element('#submit').click()
+    def fill_curent_address(self, value):
+        browser.element('#currentAddress').type(value)
+        return self
+
+    def fill_state(self, state):
+        browser.element('#state').perform(command.js.scroll_into_view).click()
+        browser.all("[id^=react-select][id*=option]").element_by(have.exact_text(state)).click()
+        return self
+
+    def fill_city(self, city):
+        browser.element('#city').perform(command.js.scroll_into_view).click()
+        browser.all("[id^=react-select][id*=option]").element_by(have.exact_text(city)).click()
+        return self
+
+    def submitting(self):
+        browser.element('#submit').click()
+
+    def should_registered_user_with(self, full_name, email, gender, number, date_of_birth, subjects,
+                                    hobbies, image, curent_address, state_city
+                                    ):
+        browser.element('.table').all('td').even.should(
+            have.exact_texts(
+                full_name,
+                email,
+                gender,
+                number,
+                date_of_birth,
+                subjects,
+                hobbies,
+                image,
+                curent_address,
+                state_city,
+            )
+        )
+        return self
--- tests/test_form_registaration_page.py
+++ tests/test_form_registaration_page.py
@@ -1,6 +1,5 @@
 import time
 from pages.registration_form import RegistrationPage
-
 registration_page = RegistrationPage()
 
 
@@ -15,4 +14,21 @@ def test_form_registration_page():
     registration_page.fill_subjects('English')
     registration_page.choose_hobbies()
     registration_page.upload_image('Lisa.bmp')
+    registration_page.fill_state('NCR')
+    registration_page.fill_curent_address('Novgorod')
+    registration_page.fill_city('Delhi')
+    registration_page.submitting()
     time.sleep(5)
+    registration_page.should_registered_user_with(
+            'Vladimir Serov',
+            'test@mail.ru',
+            'Male',
+            '8800555353',
+            '21 May,1996',
+            'English',
+            'Sports, Reading, Music',
+            'Lisa.bmp',
+            'Novgorod',
+            'NCR Delhi',
+        )
+
